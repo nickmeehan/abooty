1643a07eb491a9861dfd5d5abbaed73e246751cb
Drew West <drewfwest@gmail.com>
2014-04-25 16:17:52 -0700

    Tests pass and new routes to match Twitter format

--- app/controllers/favorites.rb
+++ app/controllers/favorites.rb
@@ -1,4 +1,4 @@
-post '/favorite/new/:aboot_id' do
+post '/favourites/new/:aboot_id' do
   Favorite.create(user: User.find(session[:user_id]), aboot: Aboot.find(params[:aboot_id]))
   redirect '/aboots'
 end
