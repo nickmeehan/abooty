05aaf80a44d8c7ca4a44452ab68d3156a3619aa2
James Hulley <james.hulley@gmail.com>
2014-04-24 20:15:58 -0700

    Added shovel method for reboot model

--- app/models/reboot.rb
+++ app/models/reboot.rb
@@ -2,4 +2,13 @@ class Reboot < ActiveRecord::Base
   belongs_to :user
   belongs_to :aboot
   belongs_to :reboot
+
+  def <<(re_item)
+    if re_item.instance_of? Aboot
+      self.aboot << re_item
+    elsif re_item.instance_of? Reboot
+      self.reboot << re_item
+      self.aboot << re_item.aboot
+    end
+  end
 end
