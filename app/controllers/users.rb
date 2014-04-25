a0a2c28db889dcde747f6135e779c61a0ee2ce8b
Chris Yancey <Chris@Chriss-MacBook-Pro-2.local>
2014-04-25 16:50:55 -0700

    fixed routes

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -18,12 +18,3 @@ post '/unfollow' do
 	User.find(params[:user_id]).followers.destroy(User.find(session[:user_id]))
 	redirect "/users/#{params[:user_id]}"
 end
-
-# get '/results' do
-#   if params[:user_input]
-#     search_terms = params[:user_input].downcase
-#     @users = User.where("de_handle LIKE ? or de_name LIKE ?", "%#{search_terms}%", "%#{search_terms}%")
-#     # @users = User.all
-#   end
-#   erb :users
-# end
