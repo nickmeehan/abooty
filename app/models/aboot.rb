95aa46bcae0a10086fb30bcf6477f838036b8b31
Chris Yancey <Chris@Chriss-MacBook-Pro-2.local>
2014-04-24 21:12:15 -0700

    Added validation to ensure a user can only favorite any given aboot once

--- app/models/aboot.rb
+++ app/models/aboot.rb
@@ -1,6 +1,8 @@
 class Aboot < ActiveRecord::Base
   belongs_to :user
   has_many :favorites
+
+
   validates :content, presence: true
   validates :content, length: { maximum: 140 }
   validates :user_id, presence: true
