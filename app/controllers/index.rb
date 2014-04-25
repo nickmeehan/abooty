301bf35cbfb4a261bd45ea1314a0a0b8fd2052f7
Chris Yancey <Chris@Chriss-MacBook-Pro-2.local>
2014-04-24 21:46:05 -0700

    Added parital for favorites list which shows all tweets a user has favorited. Added route for users/favorites which shows list of users favorite tweets

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,7 +3,15 @@ get '/aboots' do
   erb :_home_page
 end
 
+get '/users/favorites' do
+  @user = User.find(session[:user_id])
+  @favorites = @user.favorites
+  erb :_favorites_list
+end
+
 get '/users/:id' do
   @user = User.find(session[:user_id])
   erb :_profile_page
 end
+
+
